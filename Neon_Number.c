8d7c28540a99ee6c1ba4fb9b5740772fbeff3834
Jyothulachakraveni <102216487+Jyothulachakraveni@users.noreply.github.com>
2022-11-08 22:19:32 +0530

    #include<stdio.h> int main() {     int num,i;     scanf("%d",&num);
        int sqr=num*num;
         //Sum of digit
        int sum=0,rem;
        while(sqr!=0)
        {
            rem=sqr%10;
            sum+=rem;
            sqr/=10;
        }
    
       if(sum==num)
        printf("Neon Number");
       else
        printf("Not Neon Number");
    }

---
+++ Neon_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int num,i;
+    scanf("%d",&num);
+
+    int sqr=num*num;
+     //Sum of digit
+    int sum=0,rem;
+    while(sqr!=0)
+    {
+        rem=sqr%10;
+        sum+=rem;
+        sqr/=10;
+    }
+
+   if(sum==num)
+    printf("Neon Number");
+   else
+    printf("Not Neon Number");
+}
